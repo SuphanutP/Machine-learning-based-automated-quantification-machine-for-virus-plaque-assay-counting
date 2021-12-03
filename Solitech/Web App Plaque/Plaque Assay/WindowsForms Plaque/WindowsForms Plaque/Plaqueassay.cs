254b682de5e51f612d9cdaa193cae40f0abb1c33
Suphanut Plengkham <suphanutplengkham@Suphanuts-MacBook-Pro.local>
2021-12-03 16:40:35 +0700

    Adding files and modify

---
+++ Solitech/Web App Plaque/Plaque Assay/WindowsForms Plaque/WindowsForms Plaque/Plaqueassay.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowsForms_Plaque
+{
+    internal class Plaqueassay
+    {
+        public string pid { get; set; }
+        public int count { get; set; }
+        public string img { get; set; }
+    }
+}
---
+++ Solitech/Web App Plaque/Plaque Assay/WindowsForms Plaque/WindowsForms Plaque/Uploaddata.cs
@@ -0,0 +1,11 @@
+﻿using HalconDotNet;
+namespace WindowsForms_Plaque
+{
+    internal class Uploaddata
+    {
+        public int UIndex { get; set; }
+        public string Image { get; set; }
+        public HTuple ImageName { get; set; }
+        public int count { get; set; }
+    }
+}
